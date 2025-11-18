ee20d144ffe6ee0ab8448d09b83562d5adfeb7bd
dxminique <do.cofre@duocuc.cl>
2025-11-18 15:22:06 -0300

    Agregar repository de Producto

--- src/main/java/com/huertohogar/ms_productos/repository/ProductoRepository.java
+++ src/main/java/com/huertohogar/ms_productos/repository/ProductoRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-    List<Producto> findByActivoTrue();
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
