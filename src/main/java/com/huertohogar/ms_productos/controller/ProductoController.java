6ef4927db07a9503428c8afe8ba46f8acd49d23a
dxminique <do.cofre@duocuc.cl>
2025-11-18 14:55:29 -0300

    Agregar controller de Producto

--- src/main/java/com/huertohogar/ms_productos/controller/ProductoController.java
+++ src/main/java/com/huertohogar/ms_productos/controller/ProductoController.java
@@ -3,13 +3,51 @@ package com.huertohogar.ms_productos.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.huertohogar.ms_productos.model.Producto;
+import com.huertohogar.ms_productos.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
 
+    private final ProductoService productoService;
+
+    //prueba para conectar con el gateway xd
     @GetMapping("/ping")
     public String ping() {
         return "ms-productos OK";
+    }
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> getAll() {
+        return productoService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Producto getById(@PathVariable Long id) {
+        return productoService.getById(id);
+    }
+
+    @PostMapping
+    public Producto create(@RequestBody Producto producto) {
+        return productoService.create(producto);
+    }
+
+    @PutMapping("/{id}")
+    public Producto update(@PathVariable Long id, @RequestBody Producto datos) {
+        return productoService.update(id, datos);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productoService.delete(id);
     }
 }
