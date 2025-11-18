bc6aa304efd14da82154bc4463815d19a0018ea1
dxminique <do.cofre@duocuc.cl>
2025-11-18 15:34:18 -0300

    se modificaron algunas cosas en  repository y application

--- src/main/java/com/huertohogar/ms_productos/MsProductosApplication.java
+++ src/main/java/com/huertohogar/ms_productos/MsProductosApplication.java
@@ -2,10 +2,15 @@ package com.huertohogar.ms_productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.huertohogar.ms_productos.repository")
+@EntityScan(basePackages = "com.huertohogar.ms_productos.model")
+public class MsProductosApplication {
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
-public class MsProductosApplication { public static void main(String[] args) {
-	SpringApplication.run(MsProductosApplication.class, args);
-} }
-
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductosApplication.class, args);
+	}
+}
--- src/main/java/com/huertohogar/ms_productos/repository/ProductoRepository.java
+++ src/main/java/com/huertohogar/ms_productos/repository/ProductoRepository.java
@@ -1,8 +1,10 @@
 package com.huertohogar.ms_productos.repository;
 import com.huertohogar.ms_productos.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+@Service
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
 }
