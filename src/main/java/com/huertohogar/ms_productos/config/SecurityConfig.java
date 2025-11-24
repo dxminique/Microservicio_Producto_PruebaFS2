08086c9cda1229bb2015cbecb72c28728238d6a9
dxminique <do.cofre@duocuc.cl>
2025-11-24 10:43:52 -0300

    se modifico el securityConfig

--- src/main/java/com/huertohogar/ms_productos/config/SecurityConfig.java
+++ src/main/java/com/huertohogar/ms_productos/config/SecurityConfig.java
@@ -8,15 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()   // todo abierto por ahora
-                )
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
